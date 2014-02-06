3e454a8902c472544e149a670ad998f01fc0dd9c
Carl Chan-Aldebol <ccaldebol@athenahealth.com>
2014-02-06 13:00:06 -0500

    Prime factorization

--- tests.js
+++ tests.js
@@ -1,15 +1,35 @@
-function MyClass(initializationVariable) {
-	// Initialization Goes here
-	return this;
+function factorize(n) {
+	var result = [];
+	var primes = twoton(n);
+	for (var i = 0; i < primes.length; ++i) {
+		var p = primes[i];
+		while(!(n % p)){
+			n /= p;
+			result.push(p);
+		}
+	}
+
+	return result;
 }
 
-MyClass.prototype.myFunction = function() {
-	// Do Stuff
+function twoton(n) {
+	var index = 2,
+		range = [];
+	while (index <= n) {
+		range.push(index);
+		++index;
+	}
+
+	return range;
 }
-
-
 
 test("sample test", function() {
-	ok(game);
-	equal("Actual", "Expected");
+	deepEqual(factorize(2), [2]);
+	deepEqual(factorize(3), [3]);
+	deepEqual(factorize(4), [2,2]);
+	deepEqual(factorize(5), [5]);
+	deepEqual(factorize(6), [2,3]);
+	deepEqual(factorize(8), [2,2,2]);
+	deepEqual(factorize(9), [3, 3]);
+	deepEqual(factorize(42), [2, 3, 7]);
 });
